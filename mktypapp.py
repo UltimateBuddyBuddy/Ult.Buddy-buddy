2241236eb0af235ce8407ae8ea5531e60412f4c5
Birdman <ravenb27@brandeis.edu>
2017-11-08 15:11:52 -0500

    added pages

--- mktypapp.py
+++ mktypapp.py
@@ -22,6 +22,25 @@ def buddy():
 def formdemo():
 	return render_template('formdemo.html')
 
+@app.route('/keefa')
+def keefa():
+	return render_template('keefa.html')
+
+@app.route('/agnes')
+def agnes():
+	return render_template('agnes.html')
+
+@app.route('/kareem')
+def kareem():
+	return render_template('kareem.html')
+
+@app.route('/ali')
+def ali():
+	return render_template('ali.html')
+
+@app.route('/raven')
+def raven():
+	return render_template('raven.html')
 
 if __name__ == '__main__':
     app.run('0.0.0.0',port=3000)
